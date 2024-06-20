43b387e88fc376e91c63e648fe6386ef2807c79c
fengxi <fengxi@tencent.com>
2024-06-20 13:49:44 +0800

    初始版本

--- main.go
+++ main.go
@@ -4,25 +4,24 @@ package main
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"time"
 
 	"github.com/idaaser/syncdemov1/server"
 )
 
 func main() {
 	srv := server.New(8000,
+		server.WithJWTAuthnStore(
+			generateRSAKey(),
+			30*time.Minute,
+			"client_id_1", "client_secret_1",
+			"client_id_2", "client_secret_2",
+		),
 		server.WithContactFileStore(
 			"./server/testdata/departments.json",
 			"./server/testdata/users.json",
 		),
 	)
-	/**
-	server.WithJWTAuthnStore(
-		generateRSAKey(),
-		30*time.Minute,
-		"client_id_1", "client_secret_1",
-		"client_id_2", "client_secret_2",
-	),
-	*/
 
 	srv.Start()
 }
